50b390b7307b0551ee40eb066d7f5dee3abddcba
Ivan Stanevich <ivan_stanevich@waters.com>
2024-02-04 21:36:51 +0100

    replace path

--- commands/up.mjs
+++ commands/up.mjs
@@ -6,7 +6,7 @@ const ROOT_DIR = '/';
 export const goUp = () => {
     const {workDirectory} = state.getState();
 
-    if (state.getState().workDirectory !== '/') {
+    if (state.getState().workDirectory !== ROOT_DIR) {
         state.setState({workDirectory: path.resolve(workDirectory + '/../')});
         console.log(state.getState().workDirectory);
     }
